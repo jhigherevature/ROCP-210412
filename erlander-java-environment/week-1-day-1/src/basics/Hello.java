dfaf1992e89dd7b0b7ce010f974f503045047327
mrerlander <mrerlander@gmail.com>
2021-04-12 14:14:41 -0500

    refactoring for different workspace

--- erlander-java-environment/week-1-day-1/src/basics/Hello.java
+++ erlander-java-environment/week-1-day-1/src/basics/Hello.java
@@ -1,7 +1,11 @@
 package basics;
 
 public class Hello {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World...again");
+		
+		System.out.println("Hello World...again.");
+		
 	}
+
 }
