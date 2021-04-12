c5566891d4b5a9ae6892360196a587712b111832
justinvega11 <vega22@purdue.edu>
2021-04-12 14:03:16 -0500

    exercise1 day 1

---
+++ exercises/week_1/day-1/exercises-justin/src/exercises/Datatypes.java
@@ -0,0 +1,25 @@
+package exercises;
+
+public class Datatypes {
+	public static void main(String[] args) {
+
+		byte varByte = 32;
+		System.out.println(varByte);
+		short varShort = 33;
+		System.out.println(varShort);
+		char varChar = 'a';
+		System.out.println(varChar);
+		int varInt = 6;
+		System.out.println(varInt);
+		long varLong = 90000L;
+		System.out.println(varLong);
+		float varFloat = 32.09F;
+		System.out.println(varFloat);
+		double varDouble = 65526;
+		System.out.println(varDouble);
+		boolean varBoolean = true;
+		System.out.println(varBoolean);
+		
+		
+	}
+}
--- exercises/week_1/day-1/exercises-justin/src/exercises/Hello.java
+++ exercises/week_1/day-1/exercises-justin/src/exercises/Hello.java
@@ -1,4 +1,4 @@
-package exercise1;
+package exercises;
 
 public class Hello {
 	public static void main(String[] args) {
