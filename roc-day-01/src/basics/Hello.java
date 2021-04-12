e5b3cb21e21a701572387c350f9f74268f900cd8
Anthony Co <anthonyashco@gmail.com>
2021-04-12 11:58:13 -0700

    Add Hello.java

---
+++ roc-day-01/src/basics/Hello.java
@@ -0,0 +1,14 @@
+package basics;
+	// single-line comment
+
+	/* 
+	 * multi-line comment
+	 * which spans multiple lines
+	 * until the terminator
+	 */
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
