f02c96c96e66f38930912128a4ab99db9a484b52
justinvega11 <vega22@purdue.edu>
2021-04-12 12:48:40 -0500

    basic file

---
+++ justin-environment/roc-day1/src/basics/Hello.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Hello {
+	public static void main(String[] args) {
+		//single line comment
+		/*multiline
+		 * 
+		 */
+		
+		System.out.println("Hello World!");
+	}
+}
