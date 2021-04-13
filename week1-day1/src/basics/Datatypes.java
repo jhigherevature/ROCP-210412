3467c2c9538f8e6536396c6512160e92a547c45d
Jommy Abraham <abrsys@yahoo.com>
2021-04-13 09:35:26 -0500

    test

---
+++ week1-day1/src/basics/Datatypes.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// byte short char int long float double boolean 
+		
+		byte num = 127;
+		System.out.println("data type byte num = " + num);
+		
+		short s = 32767; 
+		System.out.println("data type short s = " + s);
+		
+		char ch = 'Z';
+		System.out.println("data type char ch = " + ch);
+		
+		int i = 872112;
+		System.out.println("data type int i = " + i);
+		
+		long l = 314422445L;
+		System.out.println("data type long l = " + l);
+		
+		float f = 2323.2f;
+		System.out.println("data type float f = " + f);
+		
+		double d = 232323.3434;
+		System.out.println("data type double = " + d);
+		
+		boolean b1 = true;
+		System.out.println(b1);
+	}
+
+}
---
+++ week1-day1/src/basics/Person.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// name(string), address(string), id(long), age(int)
+            String name = "Jommy Abraham";
+            String address = "212 North Blvd, Fort Worth";
+            long id = 1321313L;
+            int age = 28;
+            
+            System.out.println("Name = " + name);
+            System.out.println("Address = " + address);
+            System.out.println("Id = " + id);
+            System.out.println("Age = " + age);
+	}
+
+}
