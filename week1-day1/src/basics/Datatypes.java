f850197e24b09dab3efafb11c3a62e1abe038b41
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-12 21:23:16 -0500

    Finished first assignment

---
+++ week1-day1/src/basics/Datatypes.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class Datatypes {
+ 
+	public static void main(String[] args) {
+	byte b = 10;
+	short s = 1000;
+	char ch = 100;
+	int a = 100;
+	long num1 = 10L;
+	float num2 = 5.5f;
+	double num3 = 5.5;
+	boolean bool = true;
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(ch);
+	System.out.println(a);
+	System.out.println(num1);
+	System.out.println(num2);
+	System.out.println(num3);
+	System.out.println(bool);
+}
+}
---
+++ week1-day1/src/basics/Person.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Person {
+   String name = "John Smith";
+   String address = "ABC Street, Chicago IL";
+   long id = 0123456l;
+   int age = 45;
+   
+}
