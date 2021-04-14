ccc8c03cf7907d68c12e2b9bb9cf0e981d0144dd
nrdmraju <rahuldamaraju@Rahuls-MacBook-Pro.local>
2021-04-13 20:08:16 -0500

    completed day 1 exercies.

---
+++ neil's-roc-environment/Week 1/day1-exercises/src/Datatypes.java
@@ -0,0 +1,20 @@
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+
+		byte b = 120;
+		short s = 12345;
+		int i = 1_000_000;
+		long l = 1_000_000_000l;
+		float f = 1_000_000_000.00f;
+		double d = 1_000_000_000.00;
+		boolean bool = true;
+		char c = 'N'; 
+		
+		System.out.println("Primitive Datatypes include:");
+		System.out.println("Byte: " + b + ", " + "Short: " + s + ", " + "Int: " + i + ", " +
+		"Long: " + l + ", " + "Float: " + f + ", " + "Double: " + d + ", " + "Boolean: " + bool + ", "
+		+ "Char: " + c);
+	}
+}
---
+++ neil's-roc-environment/Week 1/day1-exercises/src/Person.java
@@ -0,0 +1,8 @@
+
+public class Person {
+	
+	public String name;
+	public String address;
+	public int id;
+	public int age;
+}
