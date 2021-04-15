45271ed51193db4cb920829f94e9fe3bd4c1da70
Jommy Abraham <abrsys@yahoo.com>
2021-04-15 16:18:47 -0500

    Adding new version for Parrot Class

---
+++ week1-day2/src/moremethods/Parrot1.java
@@ -0,0 +1,16 @@
+package moremethods;
+
+public class Parrot1 {
+    
+	static String name = "Parrot";
+	
+    public static void main(String[] args) {
+    	talk1();
+	}
+ 
+    public static void talk1() {
+        System.out.println(name + " wants a cracker");
+    } 
+}
+
+   
