6886ba4483997a7bc76f839d31aa5b4f7d255b62
Jommy Abraham <abrsys@yahoo.com>
2021-04-14 22:41:54 -0500

    Day2 exercise for methods

--- week1-day2/src/moremethods/Parrot.java
+++ week1-day2/src/moremethods/Parrot.java
@@ -1,16 +1,13 @@
 package moremethods;
 
-import java.security.PublicKey;
-
 public class Parrot {
 
 	public static void main(String[] args) {
 		String name = "Parrot";
-		
+	
+		Talk bird = new Talk();
+		bird.birdtalk = "wants a cracker!";
+		System.out.println(name + " " + bird.birdtalk);
+	
 	}
-		
-	public static void talk() {
-		System.err.println("wants a cracker");
-	}
-
 }
---
+++ week1-day2/src/moremethods/Talk.java
@@ -0,0 +1,5 @@
+package moremethods;
+
+public class Talk {
+    public String birdtalk;
+}
