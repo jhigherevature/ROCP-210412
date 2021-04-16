d9ee9fc07bb10481f3f86c203a2ca7e57f773530
Anthony Co <anthonyashco@gmail.com>
2021-04-16 16:42:08 -0700

    Push "day 4" assignments.

--- anthony-environment/aco-week1-day4/src/exceptions/Exercise1.java
+++ anthony-environment/aco-week1-day4/src/exceptions/Exercise1.java
@@ -1,5 +1,20 @@
 package exceptions;
 
 public class Exercise1 {
+    public static int addEven(int a, int b) throws OddNumberException {
+        if (a % 2 != 0 || b % 2 != 0) {
+            throw new OddNumberException("Input value was odd.");
+        } else {
+            return (a + b);
+        }
+    }
 
+    public static void main(String[] args) {
+        try {
+            int sum = addEven(4, 3);
+            System.out.println(sum);
+        } catch (OddNumberException e) {
+            e.printStackTrace();
+        }
+    }
 }
