1395392cd35c1967c74c1a27e3f19d1d700194f2
Ben Alvarado <balvarado347@gmail.com>
2021-04-18 06:12:46 -0500

    Exercises

---
+++ rocp-environment/testing/src/flowcontrol/ForLoops.java
@@ -0,0 +1,17 @@
+// Exercise: Day 2 
+
+// Description: Create an array of ten byte values. The values can be any arbitrary numbers that you choose. Now use a for loop to iterate over the elements in the array and print them to the console. //
+
+// Confusion: 
+package flowcontrol;
+
+public class ForLoops {
+
+	public static void main(String args[]) {
+		
+		// creating 10 byte array
+		byte [] array = new byte [] {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println(array);
+	}
+}
