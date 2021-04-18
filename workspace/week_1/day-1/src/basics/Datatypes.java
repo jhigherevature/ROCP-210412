c3ee1f3629b1ef2bbcdf902c18b81c4473c1d915
Alex Crowe <alexcwolf5@gmail.com>
2021-04-18 12:53:12 -0700

    day-1

---
+++ workspace/week_1/day-1/src/basics/Datatypes.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class Datatypes {
+		public static void main(String[] args) {
+	byte b = 123;
+	short s = 12345;
+	int i = 1_000_000;
+	long l = 1_000_000_000l;
+	float f = 1_000_000.01f;
+	double d = 1_000_000_000.01;
+	boolean bool = true;
+	char a = 'a';
+
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(i);
+	System.out.println(l);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(bool);
+	System.out.println(a);
+		}
+}
