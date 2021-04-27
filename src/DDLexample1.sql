254ad20fe974622267db5ef3dec3b210a59e553a
melanie <melanieduah@gmail.com>
2021-04-27 04:00:26 -0500

    DDL, DML and day5

---
+++ src/DDLexample1.sql
@@ -0,0 +1,91 @@
+--
+CREATE TABLE writer(
+writer_id int PRIMARY KEY,
+writer_name varchar(50)
+);
+
+CREATE TABLE books(
+book_id int PRIMARY KEY,
+title varchar(50),
+book_publicaton int,
+page_count int,
+writer_id int,
+CONSTRAINT writer_id_fk FOREIGN KEY (writer_id) REFERENCES writer(writer_id)
+);
+--
+--
+--INSERT INTO writer (writer_id ,writer_name) VALUES('1001','Agatha Christie');
+--INSERT INTO writer (writer_id ,writer_name) VALUES('1002','George Orwell');
+--INSERT INTO writer (writer_id ,writer_name) VALUES('1003','Kurt Vonnegut');
+--INSERT INTO writer (writer_id ,writer_name) VALUES('1004','Mark Twain');
+--INSERT INTO writer (writer_id ,writer_name) VALUES('1005','Stephen King');
+--
+--
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3050','Murder on the Orient Express','1934','256','1001') ;
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3051','It','1986','1138','1005');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3052','And Then There Were None','1939','272','1001');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3053','Pet Sematary','1983','373','1005');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3054','Slaughterhouse-Five','1969','215','1003');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3055','Nineteen Eighty-Foure','1949','1949','1002');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3056','Adventures of Huckleberry Finn','1884','366','1004');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3057','Death on the Nile','1937','288','1001');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3058','Animal Farm','1945','112','1002');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3059','The Adventures of Tom Sawyer','1876','274','1004');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3060','The Shining','1977','447','1005');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3061','Salem`s Lot','1975','439','1005');
+--INSERT INTO books (book_id ,title,book_publicaton ,page_count,writer_id) VALUES('3062','TCat`s Cradle','1963','304','1003');
+--
+--
+SELECT * FROM books;
+
+
+SELECT * FROM author;
+
+DELETE FROM books;
+
+DROP TABLE books 
+
+
+ALTER TABLE "books" RENAME COLUMN "book_id" TO "isbn_13";
+
+
+ALTER TABLE writer RENAME TO author;
+ALTER TABLE "author" RENAME COLUMN "writer_id" TO "author_id";
+ALTER TABLE "author" RENAME COLUMN "writer_name" TO "author_name";
+
+
+
+
+--
+--CREATE TABLE principal(
+--principal_id serial PRIMARY KEY,
+--principal_name varchar(50),
+--hire_date date,
+--salary decimal
+--);
+--
+--CREATE TABLE schools(
+--school_id int PRIMARY KEY,
+--school_name varchar(50),
+--principal_id int,
+--CONSTRAINT principal_id_fk FOREIGN KEY (principal_id) REFERENCES principal(principal_id)
+--);
+--
+--CREATE TABLE students(
+--student_id int PRIMARY KEY,
+--student_name varchar(50),
+--home_address varchar(50),
+--contact_phone int,
+--grade int ,
+--school_id int,
+--CONSTRAINT school_id_fk FOREIGN KEY (school_id) REFERENCES schools(school_id)
+--);
+--
+--SELECT *FROM principal;
+--SELECT *FROM schools;
+--SELECT *FROM students;
+
+
+
+
+
---
+++ src/day5/bonus2.sql
@@ -0,0 +1,97 @@
+
+CREATE TABLE state(
+state_id int PRIMARY KEY,
+state_name varchar(50)
+);
+
+CREATE TABLE team (
+term_id int PRIMARY KEY,
+team_name varchar(50),
+state_id int,
+sport_id int,
+CONSTRAINT state_id_fk FOREIGN KEY (state_id) REFERENCES state(state_id),
+CONSTRAINT sport_id_fk FOREIGN KEY (sport_id) REFERENCES sport(sport_id)
+);
+
+
+CREATE TABLE Player(
+player_id int PRIMARY KEY,
+player_name varchar(50),
+term_id int,
+player_salary decimal,
+CONSTRAINT term_id_fk FOREIGN KEY (term_id) REFERENCES Team(term_id)
+);
+
+
+CREATE TABLE sport(
+sport_id int PRIMARY KEY,
+sport_name varchar(50)
+);
+
+
+INSERT INTO state (state_id ,state_name) VALUES('10001','New York');
+INSERT INTO state (state_id ,state_name) VALUES('10002','Texas');
+INSERT INTO state (state_id ,state_name) VALUES('10003','Colorado');
+INSERT INTO state (state_id ,state_name) VALUES('10004','Florida');
+INSERT INTO state (state_id ,state_name) VALUES('10005','California');
+
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20001','Los Angeles Clippers','10005','40002');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20002','Denver Broncos','10003','40001');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20003','New York Knicks','10001','40002');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20004','Miami Dolphins','10004','40001');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20005','Denver Nuggets','10003','40002');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20006','Dallas Mavericks','10002','40002');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20007','Dallas Cowboys','10002','40001');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20008','San Francisco 49ers','10005','40001');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('20009','Miami Hea','10004','40002');
+INSERT INTO team (term_id ,team_name,state_id,sport_id) VALUES('200010','Buffalo Bills','10001','40001');
+
+
+-DROP TABLE team;
+
+
+INSERT INTO sport (sport_id ,sport_name) VALUES('40001','Foot Ball');
+INSERT INTO sport (sport_id ,sport_name) VALUES('40002','Basket Ball');
+
+
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('30001','20006','Terry Lennie','185000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('30002','20002','Ellis Sidney','101000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('30003','20001','Alex Meredith','236000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('30004','20003','Parker Lindsay','240000');
+INSERT INTO player (player_id,term_id,player_name,player_salary) VALUES('30005','20008','Lindsey Darian','241000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('30006','20007','Kit Stacy','220000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('30007','20003','Sammie Hadley','112000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('30008','20005','Tracey Bailey','128000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('30009','20002','Addison Garnet','105000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300010','20005','Esme Stace','146000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300011','20009','Kennedy Meredith','236000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300012','20004','Cortney Harper','168000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300013','20002','Loren Addison','189000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300014','20009','Jojo Noel','233000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300015','200010','Syd Hilary','132000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('300016','20006','Jools Francis','204000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('300017','20001','Beverly Terry','210000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('300018','20007','Sidney Raven','157000');
+INSERT INTO player (player_id,term_id ,player_name,player_salary) VALUES('300019','20006','Page Ricki','247000');
+INSERT INTO player (player_id,term_id,player_name,player_salary) VALUES('300020','20003','Palmer Beau','104000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300021','20008','Hadley Lindsey','133000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300022','20008','Yancy Cameron','220000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300023','200010','Jo Jools','140000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300024','20001','Raleigh Ricki','170000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300025','20004','Tibby Ronnie','138000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300026','20009','Jules Evelyn','175000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300027','20007','Lesley Izzy','179000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300028','20005','Eddie Peyton','129000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300029','200010','Alpha Jocelyn','215000');
+INSERT INTO player (player_id,term_id ,player_name ,player_salary) VALUES('300030','20004','Parker Emery','202000');
+
+
+SELECT * FROM player;
+
+
+SELECT min(player_salary) AS lowestSalary FROM player;
+
+SELECT max(player_salary) AS highestSalary FROM player;
+
+select player_name from player p2 where player_name LIKE 'j%' OR employee_name LIKE 'o%'order by player_name;
+
