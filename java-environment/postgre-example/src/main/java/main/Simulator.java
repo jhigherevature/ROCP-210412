a469605e783fd5674b414d54c761f10678689840
jhigherevature <joseph.highe@revature.com>
2021-04-27 08:49:00 -0500

    Adding Files before lecture regarding authentication service example

--- java-environment/postgre-example/src/main/java/main/Simulator.java
+++ java-environment/postgre-example/src/main/java/main/Simulator.java
@@ -6,13 +6,17 @@ import java.util.List;
 import dataaccessobject.EmployeeDAO;
 import dataaccessobject.EmployeeDAOImpl;
 import model.Employee;
+import services.AuthenticationService;
 
 public class Simulator {
 
 	public static void main(String[] args) {
+		Employee emp = AuthenticationService.authenticateEmployee("emplogin", "emppass");
+		System.out.println(emp);
+		
 //		EmployeeDAO eDao = new EmployeeDAOImpl();
 //		List<Employee> testList = eDao.selectAllEmployees();
-
+//
 //		for (Employee e : testList) {
 //			System.out.println(e);
 //		}
