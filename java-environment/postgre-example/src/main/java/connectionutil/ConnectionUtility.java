ff860a6073dfbfc030e29f39add17c259532879a
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2021-10-19 13:58:13 -0500

    Update ConnectionUtility.java

--- java-environment/postgre-example/src/main/java/connectionutil/ConnectionUtility.java
+++ java-environment/postgre-example/src/main/java/connectionutil/ConnectionUtility.java
@@ -11,21 +11,42 @@ import java.sql.SQLException;
 public class ConnectionUtility {
 	public static Connection getConnection() throws SQLException {
 		/*
-		 * By default the URL Template for our Postgre DB is:
-		 * jdbc:postgresql://{host}[:{port}]/[{database}]
+		 * The getConnection method of the DriverManager
+		 * uses the following syntax:
+		 * getConnection(db_url, username, password);
 		 * 
+		 * The URL Template for our Postgre DB is:
+		 * jdbc:postgresql://{host}[:{port}]/[{database}]
 		 * 	host - the url/location that contains our DB
 		 * 			In our case 'localhost'
 		 * 	port - the port number that allows for communication
 		 * 			with our DB.
 		 * 	database - the name of the database we have established
 		 * 				by default 'postgre'
+		 * 
 		 */
 		String url = "jdbc:postgresql://localhost:5432/postgres";
 		String user = "customers";
 		String pass = "password";
 		return DriverManager.getConnection(url, user, pass);
 	}
+	
+	public static Connection getConnectionProperties() throws SQLException, IOException {
+		FileInputStream fileStream = new FileInputStream("db-resources/dbconnection.properties");
+		Properties props = new Properties();
+		props.load(fileStream);
+		String url = props.getProperty("CONNECTION_URL");
+		String user = props.getProperty("CONNECTION_USERNAME");
+		String pass = props.getProperty("CONNECTION_PASSWORD");
+		return DriverManager.getConnection(url, user, pass);
+	}
+	
+	public static Connection getConnectionEnvVar() throws SQLException {
+		String url = System.getenv("CONN_URL");
+		String user = System.getenv("CONN_NAME");
+		String pass = System.getenv("CONN_PASS");
+		return DriverManager.getConnection(url, user, pass);
+	} 
 	
 	/*
 	 * We are using the following main method to test if
@@ -36,16 +57,30 @@ public class ConnectionUtility {
 		 * try-with-resources:
 		 * 		when creating a try-catch block we can create
 		 * 		objects or references in a set of parenthesis
-		 * 		immediately after the try keyword, which will
+		 * 		immediately after the keyword 'try', which will
 		 * 		establish a resource we will attempt to use 
 		 * 		prior to the execution of code within our try-catch
 		 * 		block.
 		 */
-		try (Connection conn = ConnectionUtility.getConnection()) {
-			System.out.println("The Connection Was Successful!");
-		} catch (SQLException sqlE) {
-			System.out.println("The Connection Failed!");
-			sqlE.printStackTrace();
+		try (Connection conn = ConnectionUtil.getConnectionEnvVar()){
+			System.out.println("[Env Var] The Connection was successful!"); // if this works, our connection was established
+		} catch (SQLException e) {
+			System.out.println("An Error Occured");
+			e.printStackTrace();
+		}
+		
+		try (Connection conn = ConnectionUtil.getConnectionProperties()){
+			System.out.println("[Prop File] The Connection was successful!"); // if this works, our connection was established
+		} catch (SQLException | IOException e) {
+			System.out.println("An Error Occured");
+			e.printStackTrace();
+		}		
+		
+		try (Connection conn = ConnectionUtil.getConnection()) {
+			System.out.println("[Hard Coded] The Connection was successful!"); // if this works, our connection was established
+		} catch(SQLException e) {
+			System.out.println("An Error Occured");
+			e.printStackTrace();
 		}
 	}
 }
